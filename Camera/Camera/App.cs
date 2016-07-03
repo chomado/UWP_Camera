77ecfcbaf501338e7585d5a08419e9ca8a734a01
chomado <chomado@gmail.com>
2016-07-03 16:44:22 +0900

    UWP worls

--- Camera/Camera/App.cs
+++ Camera/Camera/App.cs
@@ -11,20 +11,7 @@ namespace Camera
     {
         public App()
         {
-            // The root page of your application
-            MainPage = new ContentPage
-            {
-                Content = new StackLayout
-                {
-                    VerticalOptions = LayoutOptions.Center,
-                    Children = {
-                        new Label {
-                            HorizontalTextAlignment = TextAlignment.Center,
-                            Text = "Welcome to Xamarin Forms!"
-                        }
-                    }
-                }
-            };
+            MainPage = new NavigationPage(new MediaPage());
         }
 
         protected override void OnStart()
---
+++ Camera/Camera/MediaPage.xaml.cs
@@ -0,0 +1,44 @@
+﻿using Plugin.Media;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+
+namespace Camera
+{
+    public partial class MediaPage : ContentPage
+    {
+        public MediaPage()
+        {
+            InitializeComponent();
+        }
+
+        async void OnClicked(Object sender, EventArgs s)
+        {
+            await CrossMedia.Current.Initialize();
+            if (!CrossMedia.Current.IsCameraAvailable || !CrossMedia.Current.IsTakePhotoSupported)
+            {
+                return;
+            }
+
+            var file = await CrossMedia.Current.TakePhotoAsync(new Plugin.Media.Abstractions.StoreCameraMediaOptions
+            {
+                Directory = "Sample",
+                Name = "test.jpg"
+            });
+
+            if (file == null)
+                return;
+
+            image.Source = ImageSource.FromStream(() =>
+            {
+                var stream = file.GetStream();
+                file.Dispose();
+                return stream;
+            });
+        }
+    }
+}
